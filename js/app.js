5cb731132934d24d172506df1af32b8bf56375bd
ebittyschwinnie <elizabeth.m.clarkson@gmail.com>
2018-08-22 21:07:20 -0700

    3 random images only appear 1x per click also added img, js, css dir

--- js/app.js
+++ js/app.js
@@ -19,24 +19,49 @@ images.push(new BusMallImage('usb.jpg'));
 images.push(new BusMallImage('water_can.jpg'));
 images.push(new BusMallImage('wine_glass.jpg'));
 
+//var arrayUnique = function (images) {
+//	return arr.filter(function(item, images){
+//		return arr.indexOf(item) >= index;
+//	});
+//};
+//removes duplicates in an array---but there are no duplicates, just duplicate instances when clicking
+
+
+//function shuffle(array) {
+  //var copy = [], n = array.length, i;
+
+  // While there remain elements to shuffle…
+//  while (n) {
+
+    // Pick a remaining element…
+  //  i = Math.floor(Math.random() * n--);
+
+    // And move it to the new array.
+  //  copy.push(array.splice(i, 1)[0]);
+//  }
+
+  //return copy;
+//};
 
 function addImages() {
   var container = document.getElementById('image-container');
+  shuffleImages(images);
   container.innerText = '';
   var image = document.createElement('img');
   var index = generateRandom(0, images.length);
   console.log(index);
-  image.setAttribute('src', images[index].fileName);
+  image.setAttribute('src', 'img/'+images[0].fileName);
+  console.log(image);
   image.addEventListener('click', recordImageClick);
   container.appendChild(image);
   image = document.createElement('img');
   index = generateRandom(0, images.length);
-  image.setAttribute('src', 'images/'+images[index].fileName);
+  image.setAttribute('src', 'img/'+images[1].fileName);
   image.addEventListener('click', recordImageClick);
   container.appendChild(image);
   image = document.createElement('img');
   index = generateRandom(0, images.length);
-  image.setAttribute('src', 'images/'+images[index].fileName);
+  image.setAttribute('src', 'img/'+images[2].fileName);
   image.addEventListener('click', recordImageClick);
   container.appendChild(image);
 }
@@ -44,6 +69,17 @@ function addImages() {
 function generateRandom(min,max) {
   return Math.floor((Math.random() * (max - min)) + min);
 }
+
+function shuffleImages(images) {
+  for (var index = 0; index < images.length - 1; index++) {
+    var randomIndex = generateRandom(index + 1, images.length);
+    var bucket = images[index];
+    images[index] = images[randomIndex];
+    images[randomIndex] = bucket;
+  }
+  return images;
+}
+
 
 function recordImageClick(event) {
   console.log(event.target.src);
